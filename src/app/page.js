03544173e2502785af9b5364d46322d6722b92da
sanuda <sanuda@gmail.com>
2023-12-11 21:39:29 +0530

    create header

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Header from "@/components/layout/Header";
+import Hero from "@/components/layout/Hero";
 import Image from "next/image";
 
 
 export default function Home() {
   return (
-    
+    <>
     <Header/>
+    <Hero/>
+    </>
+    
   );
 }
---
+++ src/components/layout/Hero.js
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import Right from "../icons/Right";
+export default function Hero() {
+  return (
+    <section className="grid grid-cols-2">
+      <div className="py-8">
+        <h1 className="text-4xl font-semibold">
+          Everthing is better with a Pizza
+        </h1>
+        <p className="my-4 text-gray-500">
+          Pizza is the missing piece that makes every day complete, a simple yet
+          delicious joy in life.
+        </p>
+        <div className="flex  gap-4 text-sm ">
+          <button className="bg-red-500 items-center text-white px-4 py-2 rounded-full flex gap-2 uppercase">
+            Order now
+            <Right/>
+          </button>
+
+          <button className="flex gap-2 py-2 text-gray-600 font-semibold">Learn more
+            <Right/>
+             </button>
+        </div>
+      </div>
+
+      <div className=" relative">
+        <Image
+          src={"/pizza.png"}
+          layout={"fill"}
+          objectFit={"contain"}
+          alt={"pizza"}
+        />
+      </div>
+    </section>
+  );
+}
