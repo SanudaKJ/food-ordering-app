977281dafc300146c4712c357a4262cb3cc16ae2
sanuda <sanuda@gmail.com>
2023-12-12 20:25:53 +0530

    style/header

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -6,13 +6,13 @@ export default function Hero() {
     <section className="grid grid-cols-2">
       <div className="py-8">
         <h1 className="text-4xl font-semibold">
-          Everthing is better with a Pizza
+          Everthing is better with a <span className="text-red-500"> Pizza</span>
         </h1>
-        <p className="my-4 text-gray-500">
+        <p className="my-6 text-gray-500 font-semibold">
           Pizza is the missing piece that makes every day complete, a simple yet
           delicious joy in life.
         </p>
-        <div className="flex  gap-4 text-sm ">
+        <div className="flex gap-4 text-sm ">
           <button className="bg-red-500 items-center text-white px-4 py-2 rounded-full flex gap-2 uppercase">
             Order now
             <Right/>
