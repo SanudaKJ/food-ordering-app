5cd3b8d4805503378129496727787ca90c3df989
sanuda <sanuda@gmail.com>
2024-01-04 23:05:35 +0530

    Refactor nav bar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,18 +3,21 @@ export default function Header() {
   return (
     <>
       <header className="flex items-center justify-between ">
-        <Link className="text-2xl font-semibold text-red-500 " href="">
-          {" "}
-          ST PIZZA{" "}
-        </Link>
-
         <nav className="flex items-center gap-8 font-semibold text-gray-500">
-          <Link className="px-3 py-2 rounded-full hover:bg-red-500 hover:text-white" href={"/"}>Home</Link>
-          <Link className="px-3 py-2 rounded-full hover:bg-red-500 hover:text-white"  href={"/"}>Menu</Link>
-          <Link className="px-3 py-2 rounded-full hover:bg-red-500 hover:text-white"  href={"/"}>About</Link>
-          <Link className="px-3 py-2 rounded-full hover:bg-red-500 hover:text-white"  href={"/"}>Contact</Link>
+          <Link className="text-2xl font-semibold text-red-500 " href="">
+            {" "}
+            ST PIZZA{" "}
+          </Link>
+          <Link href={"/"}>Home</Link>
+          <Link href={"/"}>Menu</Link>
+          <Link href={"/"}>About</Link>
+          <Link href={"/"}>Contact</Link>
+        </nav>
+
+        <nav className="flex items-center gap-4 font-semibold text-gray-500">
+          <Link href="">Register</Link>
           <Link
-            className="px-4 py-2 text-white bg-red-500 rounded-full"
+            className="px-4 py-2 text-white bg-red-500 rounded-full hover:bg-red-600"
             href={"/"}
           >
             Login
