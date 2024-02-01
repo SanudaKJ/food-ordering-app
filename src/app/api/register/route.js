8b8bdc033f4226e2589d3e6314108bc54a6fa144
SanudaKJ <sanudajayasinghe18@gmail.com>
2024-02-01 11:29:56 +0530

    Refactor add MongoDB connection and User model

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,3 +1,12 @@
-export function POST(req) {  
-  return Response.json("Ok");
+import mongoose from "mongoose";
+import { User } from "../../models/User";
+export async function POST(req) {
+  const body = await req.json();
+  mongoose.connect(process.env.MONGO_URL);
+  const createdUser = await User.create(body);
+
+  return Response.json(createdUser);
 }
+// export function POST(req) {
+//   return Response.json("Ok");
+// }
